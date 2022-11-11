7111d8b699f04819e4675caf4f9f53f539cf800a
rick Deakins <rdeakins96@gmail.com>
2022-11-10 19:31:22 -0500

    materialize linked correctly

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
+import 'materialize-css';
+import { Button, Card, Row, Col } from 'react-materialize';
 
 function Login () {
 return(
     <div className="parentContainer">
-     <div className="containerOne">
+    <div className="containerOne">
     <div className="card small">
         <div className="card-content">
             <span className="card-title">login</span>
-                <form action="" class="login-form col">
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <div>
-                            <label for="email-login">email:</label>
-                            <input type="text" id="email-login" />
-                            </div>
-                            <div>
-                            <label for="password-login">password:</label>
-                            <input type="password" id="password-login" />
-                            </div>
-                            <div>
-                            <button className="add-btn btn-small blue z-depth-2" type="submit">login</button>
-                            </div>
+            <form action="" class="login-form col">
+                <div className="row">
+                    <div className="input-field col s6">
+                        <div>
+                        <label for="email-login">email:</label>
+                        <input type="text" id="email-login" />
+                        </div>
+                        <div>
+                        <label for="password-login">password:</label>
+                        <input type="password" id="password-login" />
+                        </div>
+                        <div>
+                        <Button className="add-btn btn-small blue z-depth-2" type="submit">login</Button>
                         </div>
                     </div>
-                </form>
+                </div>
+            </form>
         </div>
     </div>
 </div>
       
       
 <div className="containerOne">
-    <div className="card">
+    <Card className="card">
         <div className="card-content">
             <span className="card-title">Create Account</span>
                 <form className="signup-form">
@@ -47,12 +49,12 @@ return(
                     <input type="password" id="password-signup" />
                     </div>
                     <div>
-                    <button class="add-btn btn-small blue z-depth-2" type="submit">signup</button>
+                    <Button class="add-btn btn-small blue z-depth-2" type="submit">signup</Button>
                     </div>
                 </form>
             </div>
-        </div>
-    </div>
+    </Card>
+</div>
 </div>
    
 
