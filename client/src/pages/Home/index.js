6d8a99e01ad1ffe78b7b21304766233603e6f03c
Kasey <kasey_crocker@yahoo.com>
2022-11-10 20:39:21 -0500

    reorganizing the soup for Ricky

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 
 function Home() {
 return (
-<div className="container">
-{/* <div>Hello world from Login</div> */}
-
-           <div>
+	<div className="container">
+	{/* <div>Hello world from Login</div> */}
+        <div>
             <div className="card">
                 <div className="card-content">
                     <h3 className="total-calories center-align">
@@ -50,7 +49,6 @@ return (
 								<i className="fa fa-plus"></i>
 								&nbsp; Add Meal
 							</button>
-
 						</div>
 					</form>
 				</div>
