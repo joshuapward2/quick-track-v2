a47a80d0b4def7d37aca8287159370883d568229
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-11-14 22:39:41 -0500

    added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,9 +4,9 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
+
 function Header() {
 return (
-
 <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         
@@ -21,39 +21,6 @@ return (
         </nav>
       </div>
     </header>
-=======
-//<header className="bg-secondary mb-4 py-2 flex-row align-center">
-//      <div className="container flex-row justify-space-between-lg justify-center align-center">
-//        
-//       
-//        <nav className="text-center">
-//            
-//          <Link to="/">Login</Link>
-//          <Link to="/home">Home</Link>
-//          <Link to="/diet">Diet</Link>
-//          <h2>Hello World!</h2>
-//        </nav>
-//      </div>
-//    </header>
-<nav>
-    <div className="nav-wrapper">
-      <div className="brand-logo center"><i class="fa-solid fa-hippo fa-lg fa-flip"></i></div>
-      {/* <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a> */}
-      <ul className="right hide-on-med-and-down">
-        <li><a href="/home">Home</a></li>
-        <li><a href="/diet">Diet</a></li>
-        <li><a href="/Signup">Signup</a></li>
-        <li><a href="/Login">Login</a></li>
-      </ul>
-       {/* <ul class="side-nav" id="mobile-demo"> */}
-         {/* <li><a href="sass.html">Sass</a></li> */}
-         {/* <li><a href="badges.html">Components</a></li> */}
-         {/* <li><a href="collapsible.html">Javascript</a></li> */}
-         {/* <li><a href="mobile.html">Mobile</a></li> */}
-       {/* </ul> */}
-    </div>
-  </nav>
-
 
 )
 
